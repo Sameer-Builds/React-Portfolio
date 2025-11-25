19c0556cfe74d13cb93e8b17d7cfef054cb761f0
Sameer-Builds <sameerxdev@gmail.com>
2025-11-25 23:23:32 +0500

    loads data using maps

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,64 +1,55 @@
 import React from 'react'
 import {
-    FaPen,
-    FaDelicious,
-    FaVoicemail,
-    FaSquare,
-    FaSquarespace,
     FaLocationArrow,
-    FaFileMedicalAlt,
-    FaMailBulk,
-    FaMailchimp,
-    FaAmilia,
-    FaMicrophoneAltSlash,
 } from "react-icons/fa";
 import { FiMail, FiPhone } from "react-icons/fi"
+
 import Form from './Form';
 import { useSiteData } from "../context/SiteDataContext";
 
 function Contact() {
     const { contactUs } = useSiteData();
+    const iconMap = {
+        FaLocationArrow: <FaLocationArrow />,
+        FiMail: <FiMail />,
+        FiPhone: <FiPhone />,
+    };
     return (
         <>
             <div className='p-20 md:p-36 lg:p-36'>
-                <p className=' text-center text-5xl font-semibold hover:text-red-500'>{contactUs.title}</p>
-                <p className='mt-6 text-center  '>{contactUs.description}</p>
+                <p className='text-center text-5xl font-semibold hover:text-red-500'>
+                    {contactUs.title}
+                </p>
+                <p className='mt-6 text-center'>{contactUs.description}</p>
             </div>
-            <div className='pb-10 container px-6 lg:pl-22 mx-auto  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 space-x-5 space-y-5 text-center'>
-                <div className="border border-red-500/20 rounded-3xl p-10 lg:w-72 group transition-all duration-500 hover:border-red-500/60">
-                    <FaLocationArrow className="text-white/70 text-5xl mx-auto mb-5 group-hover:text-red-500 transition-colors duration-300" />
-                    <p className='relative inline-block text-white text-2xl font-bold mb-3 
-                        before:content-[""] before:absolute before:left-0 before:bottom-0 
-                        before:h-0.5 before:w-0 before:bg-red-500 before:transition-all before:duration-500 
-                        group-hover:before:w-full hover:before:w-full hover:text-red-500 group-hover:text-red-500 text-center  mx-auto'>
-                        {contactUs.cards[0].caption}
-                    </p>
-                    <p className="text-white text-center ">{contactUs.cards[0].address}</p>
-                </div>
-                <div className="border border-red-500/20 rounded-3xl p-10 lg:w-72 text-center group transition-all duration-500 hover:border-red-500/60">
-                    <FiMail className="text-white/70 text-5xl m-auto mb-5 group-hover:text-red-500 transition-colors duration-300" />
-                    <p className='relative inline-block text-white text-xl font-bold mb-3 
-                        before:content-[""] before:absolute before:left-0 before:bottom-0 
-                        before:h-0.5 before:w-0 before:bg-red-500 before:transition-all before:duration-500 
-                        group-hover:before:w-full hover:before:w-full hover:text-red-500 group-hover:text-red-500'>
-                        {contactUs.cards[1].caption}
-                    </p>
-                    <p className="text-white text-center">{contactUs.cards[1].email}</p>
-                </div>
-                <div className="border border-red-500/20 rounded-3xl p-10 lg:w-72 mb-5 text-center group transition-all duration-500 hover:border-red-500/60">
-                    <FiPhone className="text-white/70 text-5xl m-auto mb-5 group-hover:text-red-500 transition-colors duration-300" />
-                    <p className='relative inline-block text-white text-xl font-bold mb-3 
-                        before:content-[""] before:absolute before:left-0 before:bottom-0 
-                        before:h-0.5 before:w-0 before:bg-red-500 before:transition-all before:duration-500 
-                        group-hover:before:w-full hover:before:w-full hover:text-red-500 group-hover:text-red-500'>
-                        {contactUs.cards[2].caption}
-                    </p>
-                    <p className="text-white">{contactUs.cards[2].number}</p>
-                </div>
+
+            <div className='pb-10 container px-6 lg:pl-22 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 text-center'>
+
+                {contactUs.cards.map((card, index) => (
+                    <div
+                        key={index}
+                        className="border border-red-500/20 rounded-3xl p-10 lg:w-72 group transition-all duration-500 hover:border-red-500/60 mx-auto"
+                    >
+                        <div className="flex justify-center text-white/70 text-5xl mx-auto mb-5 group-hover:text-red-500 transition-colors duration-300">
+                            {iconMap[card.icon]}
+                        </div>
+                        <p className='relative inline-block text-white text-2xl font-bold mb-3 
+                            before:content-[""] before:absolute before:left-0 before:bottom-0 
+                            before:h-0.5 before:w-0 before:bg-red-500 before:transition-all before:duration-500 
+                            group-hover:before:w-full hover:before:w-full hover:text-red-500 group-hover:text-red-500'>
+                            {card.caption}
+                        </p>
+                        <p className="text-white">
+                            {card.address || card.email || card.number}
+                        </p>
+                    </div>
+                ))}
+
             </div>
+
             <Form />
         </>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,60 +11,19 @@ function Services() {
                 <p className='mt-6 text-center  '>{myService.description}</p>
             </div>
             <div className='container p-2 mx-auto  gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 '>
-                <div className='flex flex-col mt-5 '>
-                    <div>
-                        <div className='border border-red-500/20 rounded-3xl p-8 bg-zinc-900/70  group transition-all duration-500 hover:border-red-500/60'>
-                            <p className=' text-white text-2xl font-bold'>
-                                01. {myService.cards[0].title}
-                            </p>
-                            <p className='text-gray-400/70 mt-4'>{myService.cards[0].description}</p>
+                {myService.cards.map((card, index) => (
+                    <div key={index} className='flex flex-col mt-5 '>
+                        <div>
+                            <div className='border border-red-500/20 rounded-3xl p-8 bg-zinc-900/70  group transition-all duration-500 hover:border-red-500/60'>
+                                <p className=' text-white text-2xl font-bold'>
+                                    {String(index + 1).padStart(2, "0")}. {card.title}
+                                </p>
+                                <p className='text-gray-400/70 mt-4'>{card.description}</p>
+                            </div>
                         </div>
                     </div>
-                    <div className='mt-6'>
-                        <div className='border border-red-500/20 rounded-3xl p-8  bg-zinc-900/70   group transition-all duration-500 hover:border-red-500/60'>
-                            <p className=' text-white text-2xl font-bold'>
-                                02. {myService.cards[1].title}
-                            </p>
-                            <p className='text-gray-400/70 mt-4'>{myService.cards[1].description}</p>
-                        </div>
-                    </div>
-                    <div className='mt-6'>
-                        <div className='border border-red-500/20 rounded-3xl p-8  bg-zinc-900/70  group transition-all duration-500 hover:border-red-500/60'>
-                            <p className=' text-white text-2xl font-bold'>
-                                03. {myService.cards[2].title}
-                            </p>
-                            <p className='text-gray-400/70 mt-4'>{myService.cards[2].description}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col mt-5 '>
-                    <div>
-                        <div className='border border-red-500/20 rounded-3xl p-8 bg-zinc-900/70  group transition-all duration-500 hover:border-red-500/60'>
-                            <p className=' text-white text-2xl font-bold'>
-                                04. {myService.cards[3].title}
-                            </p>
-                            <p className='text-gray-400/70 mt-4'>{myService.cards[3].description}</p>
-                        </div>
-                    </div>
-                    <div className='mt-6'>
-                        <div className='border border-red-500/20 rounded-3xl p-8 bg-zinc-900/70   group transition-all duration-500 hover:border-red-500/60'>
-                            <p className=' text-white text-2xl font-bold'>
-                                05. {myService.cards[4].title}
-                            </p>
-                            <p className='text-gray-400/70 mt-4'>{myService.cards[4].description}</p>
-                        </div>
-                    </div>
-                    <div className='mt-6'>
-                        <div className='border border-red-500/20 rounded-3xl p-8 bg-zinc-900/70  group transition-all duration-500 hover:border-red-500/60'>
-                            <p className=' text-white text-2xl font-bold'>
-                                06. {myService.cards[5].title}
-                            </p>
-                            <p className='text-gray-400/70 mt-4'>{myService.cards[5].description}</p>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
-
             < Form />
         </>
     )
